2a7b07b77eae6d39e72ebcfbd6db34d5379ec8b6
Vasya <vasilii.sim@gmail.com>
2025-10-08 15:09:51 +0300

    added finance entity

---
+++ src/main/java/com/vasos/bikecheck_bot/entity/Finance.java
@@ -0,0 +1,56 @@
+package com.vasos.bikecheck_bot.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "finances")
+public class Finance {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "bike_id")
+    private Bike bike;
+
+    @Column(name = "type")
+    private String type;
+
+    @Column(name = "purchase_price")
+    private Integer purchasePrice;
+
+    private Finance(){}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Bike getBike() {
+        return bike;
+    }
+
+    public void setBike(Bike bike) {
+        this.bike = bike;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Integer getPurchasePrice() {
+        return purchasePrice;
+    }
+
+    public void setPurchasePrice(Integer purchasePrice) {
+        this.purchasePrice = purchasePrice;
+    }
+}
