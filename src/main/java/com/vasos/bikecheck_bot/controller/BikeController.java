4a687b253bf0aae670599e648540ac4ef504ee9a
vasoss <lenovovasya60@gmail.com>
2025-10-23 19:42:17 +0300

    new method for upgrading bike with new component

--- src/main/java/com/vasos/bikecheck_bot/controller/BikeController.java
+++ src/main/java/com/vasos/bikecheck_bot/controller/BikeController.java
@@ -4,6 +4,7 @@ package com.vasos.bikecheck_bot.controller;
 
 import com.vasos.bikecheck_bot.dto.*;
 import com.vasos.bikecheck_bot.entity.Bike;
+import com.vasos.bikecheck_bot.entity.Component;
 import com.vasos.bikecheck_bot.service.BikeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -35,14 +36,20 @@ public class BikeController {
 
     @PostMapping("/{bikeId}/edit/install")
     public void editInstallComponentList(@PathVariable Long bikeId,
-                                     @RequestBody ComponentListDto dto){
+                                     @RequestBody EditInstallComponentListDto dto){
         bikeService.editInstallComponentList(bikeId,dto);
     }
 
     @DeleteMapping("/{bikeId}/edit/delete")
     public void editDeleteComponentList(@PathVariable Long bikeId,
-                                        @RequestBody editDeleteComponentListDto dto){
+                                        @RequestBody EditDeleteComponentListDto dto){
         bikeService.editDeleteComponentList(bikeId,dto);
+    }
+
+    @PostMapping("/{bikeId}/upgrade/install")
+    public Component upgradeInstallComponent(@PathVariable Long bikeId,
+                                             @RequestBody InstallComponentDto dto){
+        return bikeService.upgradeInstallComponent(bikeId,dto);
     }
 
     @GetMapping
